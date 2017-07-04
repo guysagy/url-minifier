b4992860859cf9b440289b847442826566b4a91e
Guy Sagy <guysagy@hotmail.com>
2017-07-04 13:47:59 -0700

    Misc. verbiage and variable renaming changes. No user visible app behavior changes.

--- code/src/com/guysagy/miniurl/MiniUrlServlet.java
+++ code/src/com/guysagy/miniurl/MiniUrlServlet.java
@@ -18,10 +18,6 @@ import com.google.gson.JsonObject;
 
 @SuppressWarnings({ "serial", "unused" })
 
-/*
- * Documentation for Class BaseEncoding used : http://google.github.io/guava/releases/22.0-android/api/docs/
- */
-
 public class MiniUrlServlet extends HttpServlet {
 
     private MminiUrlsStorge miniUrlsStorge = new MminiUrlsStorge();
@@ -39,42 +35,63 @@ public class MiniUrlServlet extends HttpServlet {
         private String reason;
     }
 
+    //
+    // Handler for minified Urls Get requests ; shall redirect to the long Url. 
+    //
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException  {
 
-        String destination = null;
+    	// Initialize to a default value:
+        String redirectTo = BuildServerHttpLocation(req) + "index.html#!/miniUrlNotFound";
 
         try {
 
-            isDebugMode = (req.getServerName().contains("localhost"));
+            isDebugMode = req.getServerName().contains("localhost");
+
+            // Parameter that identifies whether we got here by a click on an anchor tag within our own front end. 
+            // In that case, there is no need to invoke the URL safe browsing check on the client side.
             String client = req.getParameter("client");
 
-            String minifiedUrlCode = req.getRequestURI().substring(1).trim(); // Remove the leading "/" of the URI.
+            // http://docs.oracle.com/javaee/6/api/javax/servlet/http/HttpServletRequest.html#getRequestURI()
+            // Remove the leading "/" of the URI:
+            String minifiedUrlCode = req.getRequestURI().substring(1).trim();
+
+            // http://google.github.io/guava/releases/22.0-android/api/docs/
+            // decode(...) returns an array of ASCII codes. 
             byte[] decodedMinifiedUrlCode = BaseEncoding.base32().decode(minifiedUrlCode);
+
+            // Convert the array of ASCII codes to a String.
+            // TODO : check whether these is a library method that would do this instead of my implementation.
             String minifiedUrlRowIdAsString = "";
             for(byte urlByte: decodedMinifiedUrlCode){
                 minifiedUrlRowIdAsString += Character.toString((char)urlByte);
             }
+
+            // The string is actually an integer, returned previously by miniUrlsStorge.insertUrl(...):
             Integer minifiedUrlRowId = Integer.parseInt(minifiedUrlRowIdAsString);
             String longUrl = miniUrlsStorge.getLongUrl(minifiedUrlRowId);
+
             // Output validation:
-            // We need to take into account that a malicious user hacked our database directly.
-            // Therefore, we need to validate the longUrl before sending to the user.
+            // We need to take into account that a malicious user hacked our database directly after 
+            // the data was already inserted and altered it. Therefore, we need to validate the longUrl before sending to the user.
             ValidationResult validationResult = validateLongUrl(longUrl);
             if (!validationResult.isValid) {
                 longUrl = null;
             }
 
             if (longUrl == null || longUrl.length() == 0) {
-                destination = BuildServerHttpLocation(req) + "index.html#!/miniUrlNotFound";
+            	// Url not found, or is found to be invalid.
+            	// TODO: rename the web app state below to reflect this. 
+            	redirectTo = BuildServerHttpLocation(req) + "index.html#!/miniUrlNotFound";
             } else if (client != null && client.equals("minifyApp")) {
                 // client='minifyApp' is a NV pair identifying when the client is the Home page of this web app.
                 // In this case, when redirecting to the long url destination, there is no need to repeat the
                 // Safe Browsing check (it was already done when generating the minfied URL),
                 // and we can redirect directly to the destination web site.
-                destination = longUrl;
+            	redirectTo = longUrl;
             } else {
+            	// Notify the client to first check the URL it is not malicious, and only then redirect there. 
                 String urlEncoded = URLEncoder.encode(longUrl, "UTF-8");
-                destination = BuildServerHttpLocation(req) + "index.html#!/safeRedirect?destination=" + urlEncoded;
+                redirectTo = BuildServerHttpLocation(req) + "index.html#!/safeRedirect?destination=" + urlEncoded;
             }
 
             if (isDebugMode) {
@@ -83,16 +100,19 @@ public class MiniUrlServlet extends HttpServlet {
 
         } catch (Exception exception) {
 
-            destination = BuildServerHttpLocation(req) + "index.html#!/miniUrlNotFound";
+        	redirectTo = BuildServerHttpLocation(req) + "index.html#!/miniUrlNotFound";
         }
 
-        resp.sendRedirect(destination);
+        resp.sendRedirect(redirectTo);
     }
 
+    //
+    // Handler for Post requests passing long Urls requesting a minified Url in exchange. 
     // Service promise:
     // Service response data is in JSON format.
     // Response contains both 'errorString' and 'minifiedUrl' string parameters.
     // In a response, one and only one of 'errorString' and 'minifiedUrl' is non-zero length.
+    // 
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
         String errorString = "";
@@ -110,19 +130,23 @@ public class MiniUrlServlet extends HttpServlet {
             ValidationResult validationResult = validateLongUrl(longUrl);
             if (validationResult.isValid) {
                 /*
-                 * TODO: consider checking if the longUrl is already in DB. in that case, no need to re-insert.
+                 * TODO: consider checking if the longUrl is already in DB. In that case, no need to re-insert.
                  * However, at this time, I believe such a check on each and every insert is not justified
-                 * for, what I believe is more than likely only non-often, re-entry of identical longUrl's.
+                 * for what I believe is more than likely only non-often re-entry of identical longUrl's.
                  */
                 Integer minifiedUrlRowId = miniUrlsStorge.insertUrl(longUrl);
+
+                // http://google.github.io/guava/releases/22.0-android/api/docs/
                 String minifiedUrlCode = BaseEncoding.base32().encode(minifiedUrlRowId.toString().getBytes(Charsets.US_ASCII));
                 minifiedUrl = BuildServerHttpLocation(req) + minifiedUrlCode;
-                // Given the current code structure, this cannot happen,
-                // but for maintainability sake :
+
+                // Given the current code structure, this cannot happen, but for maintainability sake :
                 if (minifiedUrl.length() == 0) {
                     errorString = "Ouch. URL minification failed due to an unexpected server error. Please try again.";
                 }
+
             } else {
+
                 errorString = "Ouch. URL minification failed: " + validationResult.reason;
             }
 
@@ -146,12 +170,15 @@ public class MiniUrlServlet extends HttpServlet {
     }
 
     // Validate that the long url does not contain any JavaScript code and is not too long for processing/storing.
-    // validateLongUrl(...) does *not* validate that the input is a valid URL; it validates only that it does not contain  scripts
-    // and that it is not too long for processing/storage.
+    // validateLongUrl(...) does *not* validate that the input is a valid URL per RFC; it validates only that it 
+    // is safe to be processed by this application.
     private ValidationResult validateLongUrl(String longUrl) {
 
         ValidationResult validationResult = new ValidationResult(true, "URL is a valid URL.");
-        int maxAllowedCharacters = (isDebugMode == true) ? 100 : 4000;
+
+        // For testing ease, in dev / localhost mode, make maxAllowedCharacters a relatively small number. 
+        int maxAllowedCharacters = (isDebugMode == true) ? 100 : 4000;	
+
         if (longUrl.length() > maxAllowedCharacters) {
             validationResult.isValid = false;
             validationResult.reason = "URL exceeds max allowed characters.";
