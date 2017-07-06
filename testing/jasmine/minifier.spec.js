a2e28d65fd65d90e3618301195a0fa8e2110f363
Guy Sagy <guysagy@hotmail.com>
2017-07-05 22:12:14 -0700

    Initial commit of Jasmine test specs.

---
+++ testing/jasmine/minifier.spec.js
@@ -0,0 +1,13 @@
+describe("Testing auth module - ", function() {
+
+    beforeEach(function() {
+        //module('auth');
+        //inject(function($injector) {
+        //    authService = $injector.get('authService');
+        //});
+    });
+
+    it("dummy", function() {
+    });
+
+});
